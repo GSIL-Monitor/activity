890f4d99fe5b2d3debbefe411ce732af1dec31da
范兴宇 <fanxingyu@fanxingyudeMacBook-Pro.local>
2019-01-09 18:39:40 +0800

    add：添加modal组建

--- src/routers/Extension.tsx
+++ src/routers/Extension.tsx
@@ -1,15 +1,9 @@
-import Button from "antd-mobile/lib/button"; // 加载 JS
-import "antd-mobile/lib/button/style/css";
 import * as React from "react";
 import { connect } from "react-redux";
 import "./style.less";
 class Extension extends React.Component {
   public render() {
-    return (
-      <div className="test">
-        <Button type="primary">111</Button>
-      </div>
-    );
+    return <div className="test">111</div>;
   }
 }
 
